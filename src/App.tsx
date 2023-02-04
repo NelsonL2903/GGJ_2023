a77592016bc85e5cca4edc65bad3e6382f133b8a
Ryan Eggens <81097013+RyEggGit@users.noreply.github.com>
2023-02-04 11:43:02 -0500

    push for nelson

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import {app} from './pixi/main';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    app.start();
+  });
   return (
     <div className="App">
-      Hey Guys!
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pixi/main.ts
@@ -0,0 +1,12 @@
+import PIXI from 'pixi.js';
+
+const Application = PIXI.Application;
+
+export const app = new Application({
+    width: 256,
+    height: 256,
+    antialias: true,
+    resolution: 1
+});
+
+
