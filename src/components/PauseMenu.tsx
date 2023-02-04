5ed267ef9c0871f166856fd8732f1e4e11fb336a
Ryan Eggens <81097013+RyEggGit@users.noreply.github.com>
2023-02-04 18:20:39 -0500

    Game looking good

---
+++ src/components/PauseMenu.tsx
@@ -0,0 +1,10 @@
+const PauseMenu = (props: any) => {
+
+    return (
+        <div className='absolute flex right-4 top-4'>
+            Test
+        </div>
+    );
+};
+
+export default PauseMenu;
--- src/components/stats/Stats.tsx
+++ src/components/stats/Stats.tsx
@@ -1,7 +1,18 @@
 import StatBox from "./statBox";
 
 const StatIcons = () => {
-  return <StatBox value={100} max={100} x={1300} colour="#ff0000"></StatBox>;
+  // health, water, nitrgoen, phosophorus
+  return (
+    <div className="flex absolute right-8 bottom-8 w-96 justify-between">
+      <StatBox value={100} max={100} x={1300} colour="#ff0000" type="health"></StatBox>
+      <StatBox value={100} max={100} x={1300} colour="#ff0000" type="water"></StatBox>
+      <StatBox value={100} max={100} x={1300} colour="#ff0000" type="nitrgoen"></StatBox>
+      <StatBox value={100} max={100} x={1300} colour="#ff0000" type="phosophorus"></StatBox>
+    </div>
+
+
+
+  );
 };
 
 export default StatIcons;
