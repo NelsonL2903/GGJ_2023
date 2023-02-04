97c6de6d299c030a14d4bfeb7e0e3c8bf9bba5f6
Ryan Eggens <81097013+RyEggGit@users.noreply.github.com>
2023-02-04 15:31:41 -0500

    remove pixi

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { Stage } from '@pixi/react';
-import Background from './components/Background';
-import Sun from './components/Sun';
-
 function App() {
   return (
-    <Stage>
-      <Background />
-      <Sun />
-    </Stage>
+    <div>
+      Test
+    </div>
   );
 }
 
