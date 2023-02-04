b51e1f6414f9fa4d415e4814d71d5560d7434b4e
Benjamin <benjamin.zhao@uwaterloo.ca>
2023-02-04 18:50:56 -0500

    added music

--- src/pages/Homescreen.tsx
+++ src/pages/Homescreen.tsx
@@ -1,6 +1,5 @@
 import "../index.css";
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import VolumeToggle from "../components/LeftMenu";
 import { useDispatch } from "react-redux";
 import { setGameState } from "../store/slice";
 
@@ -9,9 +8,6 @@ const HomeScreen = () => {
     return (
 
         <div className="h-[100vh] flex flex-col justify-center items-center relative bg-blue-400">
-            <div className="absolute flex right-4 top-4">
-                <VolumeToggle />
-            </div>
             <h1 className="font-bold text-9xl text-orange-900">Old Oak</h1>
             <div className="flex justify-center items-center text-green-900 bg-orange-900 p-4 rounded-lg cursor-pointer"
                 onClick={() => dispatch(setGameState('start'))}>
