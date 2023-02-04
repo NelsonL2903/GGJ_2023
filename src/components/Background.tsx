71f7728bcb6fba27c53b52f3d88856697e98caca
Nelson Loop <nelsonploop@gmail.com>
2023-02-04 16:24:43 -0500

    Background can now have children

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,10 +1,9 @@
-
-
-const Background = () => {
-    return (
-        
-        <div className="bg-gradient-to-t from-indigo-500 to-blue-300 h-[100vh]"></div>
-    );
+const Background = (props: any) => {
+  return (
+    <div className="bg-gradient-to-t from-indigo-500 to-blue-300 h-[100vh]">
+      {props.children}
+    </div>
+  );
 };
 
 export default Background;
