12d31b3a4e84bece197189b553403313dbaf13ea
Ryan Eggens <81097013+RyEggGit@users.noreply.github.com>
2023-02-04 12:03:56 -0500

    template

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import { Stage, Container, Sprite, Text } from '@pixi/react';
+import { Stage, Container, Sprite, Text, } from '@pixi/react';
+import { TextStyle } from '@pixi/text';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
         y={270}
         anchor={{ x: 0.5, y: 0.5 }}
       />
+      <Text text={`You clicked ${count} times`} x={400} y={50} anchor={{ x: 0.5, y: 0.5 }}  />
     </Stage>
   );
 }
