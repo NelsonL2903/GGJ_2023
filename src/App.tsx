2c385e7a12be26ec76a95f9771b91110263d37a2
Benjamin <benjamin.zhao@uwaterloo.ca>
2023-02-04 15:36:04 -0500

    nuked pixi

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Stage } from '@pixi/react';
-import Background from './components/Background';
-import Sun from './components/Sun';
 
 function App() {
   return (
-    <Stage>
-      <Background />
-      <Sun />
-    </Stage>
+    <div></div>
   );
 }
 
--- src/components/Sun.tsx
+++ src/components/Sun.tsx
@@ -1,28 +1,6 @@
-import { useEffect, useState } from "react";
-import SunImg from "../assets/sun.png";
-import { Sprite, useTick } from "@pixi/react";
-
-
-
-
 
 const Sun = () => {
-    const [coords, setCoords] = useState({ x: 0, y: 200 });
-    const [anchor, setAnchor] = useState({ x: 450, y: 800 });
-
-    const angle = 1 * (Math.PI / 180);
-    useTick(() => {
-        setCoords((coords) => {
-            return {
-                x: Math.cos(angle) * (coords.x - anchor.x) - Math.sin(angle) * (coords.y - anchor.y) + anchor.x,
-                y: Math.sin(angle) * (coords.x - anchor.x) + Math.cos(angle) * (coords.y - anchor.y) + anchor.y
-            };
-        });
-    });
-
-    return (
-        <Sprite image={SunImg} x={coords.x} y={coords.y} anchor={{ x: 0.5, y: 0.5 }} scale={0.4} />
-    );
+    return null;
 };
 
 export default Sun;
