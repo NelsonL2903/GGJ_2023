9353ad87599f7caebbf2189674d2ecfc27b69bde
Nelson Loop <nelsonploop@gmail.com>
2023-02-05 10:48:58 -0500

    Set up initial pause menu

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,38 +1,37 @@
-import { useState } from 'react';
-import useSound from 'use-sound';
-import VolumeUpIcon from '@mui/icons-material/VolumeUp';
-import VolumeOffIcon from '@mui/icons-material/VolumeOff';
-import PauseIcon from '@mui/icons-material/Pause';
-import { brown } from '@material-ui/core/colors';
-
+import { useState } from "react";
+import useSound from "use-sound";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import VolumeOffIcon from "@mui/icons-material/VolumeOff";
+import { brown } from "@material-ui/core/colors";
+import PauseMenu from "./PauseMenu";
 
 const VolumeToggle = (props: any) => {
-    const [play, { stop }] = useSound('', { volume: 1.00 });
-    const [mute, setMute] = useState(false);
+  const [play, { stop }] = useSound("", { volume: 1.0 });
+  const [mute, setMute] = useState(false);
 
-    const toggleMute = () => {
-        setMute(!mute);
-        if (mute) {
-            stop();
-        } else {
-            play();
-        }
-    };
+  const toggleMute = () => {
+    setMute(!mute);
+    if (mute) {
+      stop();
+    } else {
+      play();
+    }
+  };
 
-    return (
-        <div className='absolute flex right-4 top-4'>
-            <div>
-                {
-                    props.pause ??
-                    < PauseIcon sx={{ fontSize: 40, color: brown[500] }} />
-                }
-            </div>
-            <div onClick={toggleMute}>
-                {mute ? <VolumeOffIcon sx={{ fontSize: 40, color: brown[500] }} /> : <VolumeUpIcon sx={{ fontSize: 40, color: brown[500] }} />}
-            </div>
-        </div>
-
-    );
+  return (
+    <div className="absolute flex right-4 top-4 z-20">
+      <div>
+        <PauseMenu />
+      </div>
+      <div onClick={toggleMute}>
+        {mute ? (
+          <VolumeOffIcon sx={{ fontSize: 40, color: brown[500] }} />
+        ) : (
+          <VolumeUpIcon sx={{ fontSize: 40, color: brown[500] }} />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default VolumeToggle;
--- src/components/PauseMenu.tsx
+++ src/components/PauseMenu.tsx
@@ -1,10 +1,57 @@
-const PauseMenu = (props: any) => {
+import { useState } from "react";
+import PauseIcon from "@mui/icons-material/Pause";
+import { brown } from "@material-ui/core/colors";
+import { Modal, Box, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
-    return (
-        <div className='absolute flex right-4 top-4'>
-            Test
-        </div>
-    );
+const PauseMenu = (props: any) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  return (
+    <div>
+      <PauseIcon
+        onClick={handleOpen}
+        sx={{ fontSize: 40, color: brown[500] }}
+      />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <CloseIcon
+            onClick={handleClose}
+            className="absolute top-1 right-1"
+          ></CloseIcon>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
 };
 
 export default PauseMenu;
